a1bf7d8046b6d2fb901dba0a6f08e3a2c4409cec
melih0132 <melih.cetinkaya.32@gmail.com>
2025-10-12 17:30:55 +0200

    Add path configuration to example scripts for module accessibility

--- DonatelloPyzza/examples/generate_maze.py
+++ DonatelloPyzza/examples/generate_maze.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+# Configuration du path
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.abspath(os.path.join(current_dir, ".."))
+sys.path.insert(0, parent_dir)
+
 from donatellopyzza import Game
 from donatellopyzza import Action
 from donatellopyzza import Feedback
